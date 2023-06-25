bce77cde29f23ee5bd0a691783daf8a2535898f9
DongDongEEEE <134983411+DongDongEEEE@users.noreply.github.com>
2023-06-25 22:22:18 +0900

    Delete 영화SemiProject directory

--- 영화SemiProject/3. DDL 작성/Movie_information.sql
+++
@@ -1,9 +0,0 @@
--- movie_inforamtion
-create table Movie_information(
-    movie_seq number primary key,
-    seq number references Movie(seq),
-    dir varchar2(50),
-    actor varchar2(50),
-    grade varchar2(50),
-    answer varchar2(250)
-);
--- 영화SemiProject/3. DDL 작성/Outline.sql
+++
@@ -1,9 +0,0 @@
--- Outline
-create table Outline (
-    outline_seq number primary key,
-    seq references Movie(seq),
-    genre varchar2(50),
-    area varchar2(100),
-    running number,
-    opendate date
-);
